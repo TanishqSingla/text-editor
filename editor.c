21abb813218a1f846f6e10852cfbb5fcf0221309
TanishqSingla <tanishqsingla00@gmail.com>
2021-02-25 23:28:20 +0530

    disabling raw mode after program exits

--- editor.c
+++ editor.c
@@ -1,13 +1,21 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disable_raw_mode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enable_raw_mode()
 {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disable_raw_mode); // executes when the main function returns
 
+    struct termios raw = orig_termios;
     raw.c_lflag &= ~(ECHO);
-
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
