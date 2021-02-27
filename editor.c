0207016029b8abd35e4809d8a387d5501a50c492
TanishqSingla <tanishqsingla00@gmail.com>
2021-02-28 01:04:44 +0530

    Added error handling function for every termios function

--- editor.c
+++ editor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <errno.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -14,12 +15,14 @@ void die(const char *s)
 
 void disable_raw_mode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
 void enable_raw_mode()
 {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        die("tcgetattr");
     atexit(disable_raw_mode); // executes when the main function returns
 
     struct termios raw = orig_termios;
@@ -31,7 +34,8 @@ void enable_raw_mode()
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsetattr");
 }
 
 int main()
@@ -41,7 +45,8 @@ int main()
     while (1)
     {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            die("read");
         if (iscntrl(c))
         {
             printf("%d\r\n", c);
