55dcf856cbf202684330ab03081ea1e9470d75a1
TanishqSingla <tanishqsingla00@gmail.com>
2021-03-05 23:12:36 +0530

    printing rows according to the size of terminal

--- editor.c
+++ editor.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -71,7 +72,7 @@ char editor_read_key()
 void editor_draw_rows()
 {
     int y;
-    for (y = 0; y < 24; y++)
+    for (y = 0; y < E.screenrows; y++)
     {
         write(STDOUT_FILENO, "~\r\n", 3);
     }
@@ -85,6 +86,22 @@ void editor_refresh_screen()
     editor_draw_rows();
     // Repositioning the cursor to top left
     write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
 }
 
 /*** input ***/
@@ -104,9 +121,16 @@ void editor_process_keypress()
 }
 
 /*** init ***/
+void initEditor()
+{
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+        die("getWindowSize");
+}
+
 int main()
 {
     enable_raw_mode();
+    initEditor();
 
     while (1)
     {
