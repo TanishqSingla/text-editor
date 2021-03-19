01f49b4ea3717d19a49542e5dc4f069cf0d73cae
TanishqSingla <tanishqsingla00@gmail.com>
2021-03-20 02:33:15 +0530

    attempting reading from a file

--- editor.c
+++ editor.c
@@ -172,16 +172,31 @@ int editor_read_key()
 /*** file i/o ***/
 
 // testing by showing hello world to editor
-void editorOpen()
+void editorOpen(char *filename)
 {
-    char *line = "Hello World!";
-    ssize_t linelen = 13;
+    FILE *fp = fopen(filename, "r");
+    if (!fp)
+        die("fopen");
 
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+
+    linelen = getline(&line, &linecap, fp);
+
+    if (linelen != -1)
+    {
+        while (linelen >= 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+            linelen--;
+        E.row.size = linelen;
+        E.row.chars = malloc(linelen + 1);
+        memcpy(E.row.chars, line, linelen);
+        E.row.chars[linelen] = '\0';
+        E.numrows = 1;
+    }
+
+    free(line);
+    fclose(fp);
 }
 
 /*** append buffer ***/
@@ -386,11 +401,14 @@ void initEditor()
         die("getWindowSize");
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     enable_raw_mode();
     initEditor();
-    editorOpen();
+    if (argc >= 2)
+    {
+        editorOpen(argv[1]);
+    }
 
     while (1)
     {
