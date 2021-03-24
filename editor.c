5c49528a08327a088f564550d368f875713c0f52
TanishqSingla <tanishqsingla00@gmail.com>
2021-03-24 20:13:27 +0530

    showing row number

--- editor.c
+++ editor.c
@@ -11,6 +11,7 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 /*** define ***/
@@ -368,16 +369,26 @@ void editor_draw_status_bar(struct abuf *ab)
 {
     abAppend(ab, "\x1b[7m", 4);
 
-    char status[80];
+    char status[80], rstatus[80];
     int len = snprintf(status, sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
+
     if (len > E.screencols)
         len = E.screencols;
     abAppend(ab, status, len);
 
     while (len < E.screencols)
     {
-        abAppend(ab, " ", 1);
-        len++;
+        if (E.screencols - len == rlen)
+        {
+            abAppend(ab, rstatus, rlen);
+            break;
+        }
+        else
+        {
+            abAppend(ab, " ", 1);
+            len++;
+        }
     }
     abAppend(ab, "\x1b[m", 3);
 }
